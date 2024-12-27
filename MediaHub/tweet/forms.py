42b78e514d34ed2af12df02674069acc1e0373bc
Siddhartha Pal <siddhartha.pal.official@gmail.com>
2024-12-27 17:06:54 +0530

    Add design and responsiveness

--- MediaHub/tweet/forms.py
+++ MediaHub/tweet/forms.py
@@ -4,12 +4,16 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class TweetForm(forms.ModelForm):
-  class Meta:
-    model = Tweet
-    fields = ['text', 'photo']
+    class Meta:
+        model = Tweet
+        fields = ['text', 'photo']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['photo'].widget = forms.FileInput()
 
 class UserRegistrationForm(UserCreationForm):
-  email = forms.EmailField()
-  class Meta:
-    model = User
-    fields = ('username', 'email', 'password1', 'password2')
+    email = forms.EmailField()
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
