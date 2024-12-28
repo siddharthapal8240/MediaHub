1c819069b8f0c7d5bbeef9b300a18931487375fb
Siddhartha Pal <siddhartha.pal.official@gmail.com>
2024-12-28 14:05:19 +0530

    Update

--- MediaHub/settings.py
+++ MediaHub/settings.py
@@ -28,6 +28,8 @@ DEBUG = os.environ.get('DEBUG', 'False').lower() == 'True'
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
+CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS").split(" ")
+
 
 # Application definition
 
