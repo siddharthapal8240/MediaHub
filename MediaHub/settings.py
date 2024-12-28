0c81187c9eb673144fa9580e63dff24f6b6c4900
Siddhartha Pal <siddhartha.pal.official@gmail.com>
2024-12-28 14:44:21 +0530

    Fix bug

--- MediaHub/settings.py
+++ MediaHub/settings.py
@@ -131,8 +131,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 
-if ENVIRONMENT == os.getenv('DJANGO_ENV', 'development')
-
+if ENVIRONMENT == os.getenv('DJANGO_ENV', 'development'):
     MEDIA_ROOT = BASE_DIR / 'media'
 else:
     DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
